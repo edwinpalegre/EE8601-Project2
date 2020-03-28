f8bce95d342f47dbd67aaff2f5244f333a2c914c
edwinpalegre <edwin.p.alegre@gmail.com>
2020-03-27 21:14:14 -0400

    Completed code for basic style transfer

---
+++ project_builds/image_utils.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 26 23:26:13 2020
+
+@author: ealegre
+"""
+
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from tensorflow.python.keras.preprocessing import image as tf_image
+
+
+def img_load(img_path):
+    max_dim = 1024
+    img = Image.open(img_path)
+    img_size = max(img.size)
+    scale = max_dim/img_size
+    img = img.resize((round(img.size[0]*scale), round(img.size[1]*scale)), Image.ANTIALIAS)
+    img = tf_image.img_to_array(img)
+    
+    # Image needs to be broadcasted so that it has a dimension for the batch
+    img = np.expand_dims(img, axis=0)
+    return img
+
+# def imshow(img, title=None):
+#     # Remove the batch dimension
+#     out = np.squeeze(img, axis=0)
+    
+#     # Normalize for display
+#     out = out.astype('uint8')
+#     if title is not None:
+#         plt.title(title)
+#     plt.imshow(out)
+    
+def load_process_img(img_path):
+    img = img_load(img_path)
+    img = tf.keras.applications.vgg19.preprocess_input(img)
+    return img
+
+
+def deprocess_img(processed_img):
+    x = processed_img.copy()
+    if len(x.shape) == 4:
+        x = np.squeeze(x, 0)
+    assert len(x.shape) == 3, ("Input to deprocess image must be an image of "
+                                "dimension [1, height, width, channel] or [height, width, channel]")
+    if len(x.shape) != 3:
+        raise ValueError("Invalid input to deprocessing image")
+        
+    # Perform the inverse of preprocessing stage
+    x[:, :, 0] += 103.939
+    x[:, :, 1] += 116.779
+    x[:, :, 2] += 123.68
+    x = x[:, :, ::-1]
+    
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+
+def save_results(best_img, path):
+    img = Image.fromarray(best_img)
+    img.save(path)
---
+++ project_builds/main.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 27 01:36:16 2020
+
+@author: ealegre
+"""
+
+import tensorflow as tf
+
+import image_utils as Image
+import model as Model
+
+
+content_path = 'input/toronto.jpg'
+style_path = 'input/style_starrynight.jpg'
+
+content = Image.img_load(content_path).astype('uint8')
+style = Image.img_load(style_path).astype('uint8')
+
+# Content layer where will pull our feature maps
+content_layers = ['block4_conv2']
+
+# Style layer we are interested in
+style_layers = ['block1_conv1',
+                'block2_conv1',
+                'block3_conv1',
+                'block4_conv1',
+                'block5_conv1'
+                ]
+
+best, best_loss = Model.run_style_transfer(content_path, style_path, num_iterations=1000)
+
+Image.save_results(best, 'output/output.jpg')
